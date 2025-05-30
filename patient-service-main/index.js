e8bc388496fde0540f5ce9e47b9d7d85e241675d
mansoor <mansoorali9160@gmail.com>
2025-05-30 16:21:17 +0530

    deploy app

--- patient-service-main/index.js
+++ patient-service-main/index.js
@@ -59,3 +59,5 @@ const express = require('express');
     app.listen(port, '0.0.0.0', () => {
       console.log(`Patient service listening at http://0.0.0.0:${port}`);
     });
+
+    // deploy app to ecs
